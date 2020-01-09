e9e63806cfb99ade851a79e140c66a435214ddfe
Guillaume Leon <guillaume.leon2000@gmail.com>
2020-01-09 15:30:55 +0100

    identation fix

--- autre.php
+++ autre.php
@@ -1,4 +1,6 @@
 <?php
+require('db.php');
+
 session_start();
 var_dump($_SESSION['connected']);
 ?>
@@ -6,44 +8,52 @@ var_dump($_SESSION['connected']);
 <html lang="fr">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="icon" href="image/favicon.ico" />
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/style.css">
-  <title>Creation Lab</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="image/favicon.ico" />
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Creation Lab</title>
 </head>
 
 <body>
-  <nav class="navbar">
+<nav class="navbar">
     <span class="logo"><a href="index.php"><img src="image/Creation_Lab.png" alt="logo_creationLab" width="149"
-          height="71"></a></span>
+                                                height="71"></a></span>
     <span><a href="deco.php"><button>Déconnexion</button></a></span>
-  </nav>
-<div class="container post">
-  <div class="vote text-center">
-    <button><img src="image/arrow_up.svg" alt="upvote"></button>
-    <div class="numberVote">30</div>
-    <button><img src="image/arrow_down.svg" alt="downvote"></button>
-  </div>
-<div class="corps">
-  <div class="info">
-  info (auteur, date)
-  </div>
-  <div class="title">
-    titre
-  </div>
-  <div class="contenue">
-  contenue du post
-  </div>
-  <div class="interaction">
-<button>Commenter</button>
-<button>Partager</button>
-  </div>
+</nav>
+
+<?php
+$get_post = $db->prepare('select * from users where');
+$get_post->execute();
+$get_array = $get_post->fetchAll();
+
+var_dump($get_array);
+?>
+
+<div class="container post mt-5">
+    <div class="vote text-center">
+        <button><img src="image/arrow_up.svg" alt="upvote"></button>
+        <div class="numberVote">30</div>
+        <button><img src="image/arrow_down.svg" alt="downvote"></button>
+    </div>
+    <div class="corps">
+        <div class="info">
+            info (auteur, date)
+        </div>
+        <div class="title">
+            titre
+        </div>
+        <div class="contenue">
+            contenue du post
+        </div>
+        <div class="interaction">
+            <button>Commenter</button>
+            <button>Partager</button>
+        </div>
+    </div>
 </div>
-  </div>
 </div>
 </body>
-
 </html>
