a7deb26272d436f95fbe469b1fddf031f5049014
GuillaumeLeon <guillaume@JeuneCouy.home>
2020-01-10 14:07:53 +0100

    added ajax script

--- autre.php
+++ autre.php
@@ -1,8 +1,11 @@
 <?php
-require 'db.php';
+require('db.php');
 
 session_start();
 var_dump($_SESSION['connected']);
+if($_SESSION['connected'] != 1) {
+   header('Location:index.php');
+}
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -14,44 +17,80 @@ var_dump($_SESSION['connected']);
     <link rel="icon" href="image/favicon.ico" />
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
     <title>Creation Lab</title>
+    <style type="text/css">
+        .ajax-load{
+            background: #e1e1e1;
+            padding: 10px 0px;
+            width: 100%;
+        }
+    </style>
 </head>
 
 <body>
 <nav class="navbar">
     <span class="logo"><a href="index.php"><img src="image/Creation_Lab.png" alt="logo_creationLab" width="149"
-                                                height="71"></a></span>
+						height="71"></a></span>
     <span><a href="deco.php"><button>Déconnexion</button></a></span>
 </nav>
-
+<div class="post-data">
 <?php
 $get_post = $db->prepare('SELECT * FROM post_text');
 $get_post->execute();
-$get_array = $get_post->fetchAll();
 
-var_dump($get_array);
 ?>
-<div class="container post mt-5">
-    <div class="vote text-center">
-        <button><img src="image/arrow_up.svg" alt="upvote"></button>
-        <div class="numberVote"><?php echo ($get_array[0]['upvote'] - $get_array[0]['downvote']) ?></div>
-        <button><img src="image/arrow_down.svg" alt="downvote"></button>
-    </div>
-    <div class="corps">
-        <div class="info">
-           <?php echo "Crée par " . $get_array[0]['author'] . " le " . $get_array[0]['date_post']; ?>
-        </div>
-        <div class="title">
-            <?php echo $get_array[0]['post_name'] ?>
-        </div>
-        <div class="contenue">
-           <?php echo $get_array[0]['contenue'] ?>
-        </div>
-        <div class="interaction">
-            <button>Commenter</button>
-            <button>Partager</button>
-        </div>
-    </div>
+<?php include('data.php'); ?>
 </div>
+
+<div class="ajax-load text-center" style="display:none">
+    <p><img src="http://demo.itsolutionstuff.com/plugin/loader.gif">Loading More post</p>
+</div>
+<script type="text/javascript">
+$(window).scroll(function() {
+
+   if($(window).scrollTop() + $(window).height() >= $(document).height()) {
+
+      var last_id = $(".post-id:last").attr("id");
+
+      loadMoreData(last_id);
+
+	}
+
+    });
+    function loadMoreData(last_id){
+       $.ajax(
+    {
+
+       url: '/loadMoreData.php?last_id=' + last_id,
+
+	  type: "get",
+
+	  beforeSend: function()
+
+	  {
+
+	     $('.ajax-load').show();
+
+		}
+
+	    })
+
+	    .done(function(data)
+
+	    {
+
+	       $('.ajax-load').hide();
+
+	       $("#post-data").append(data);
+
+	    })
+	    .fail(function(jqXHR, ajaxOptions, thrownError)
+	    {
+	       alert('server not responding...');
+	    });
+    }
+
+</script>
 </body>
 </html>
