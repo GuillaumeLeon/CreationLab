76cb1b939ef44a965a3c54b3d842596b8ada34e1
Guillaume Leon <guillaume.leon2000@gmail.com>
2020-01-10 23:15:52 +0100

    updated database

--- database/creationlab.sql
+++ database/creationlab.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  jeu. 09 jan. 2020 à 21:23
--- Version du serveur :  10.4.10-MariaDB
--- Version de PHP :  7.4.0
+-- Hôte : localhost
+-- Généré le :  ven. 10 jan. 2020 à 23:14
+-- Version du serveur :  10.3.18-MariaDB-0+deb10u1
+-- Version de PHP :  7.3.11-1~deb10u1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,9 +28,8 @@ SET time_zone = "+00:00";
 -- Structure de la table `post`
 --
 
-DROP TABLE IF EXISTS `post`;
-CREATE TABLE IF NOT EXISTS `post` (
-  `post_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `post` (
+  `post_id` int(11) NOT NULL,
   `post_name` varchar(255) CHARACTER SET latin1 NOT NULL,
   `post_desc` text CHARACTER SET latin1 DEFAULT NULL,
   `upvote` int(11) DEFAULT NULL,
@@ -38,16 +37,8 @@ CREATE TABLE IF NOT EXISTS `post` (
   `tag` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `author` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `slug` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
-  `date_post` date NOT NULL,
-  PRIMARY KEY (`post_id`),
-  KEY `tag` (`tag`),
-  KEY `author` (`author`),
-  KEY `post_name` (`post_name`),
-  KEY `post_desc_2` (`post_desc`(1000)),
-  KEY `upvote` (`upvote`),
-  KEY `downvote` (`downvote`),
-  KEY `slug` (`slug`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `date_post` date NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Déchargement des données de la table `post`
@@ -62,9 +53,8 @@ INSERT INTO `post` (`post_id`, `post_name`, `post_desc`, `upvote`, `downvote`, `
 -- Structure de la table `post_draw`
 --
 
-DROP TABLE IF EXISTS `post_draw`;
-CREATE TABLE IF NOT EXISTS `post_draw` (
-  `post_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `post_draw` (
+  `post_id` int(11) NOT NULL,
   `post_name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `post_desc` text COLLATE utf8_bin DEFAULT NULL,
   `contenue_url` varchar(255) COLLATE utf8_bin DEFAULT NULL,
@@ -74,8 +64,7 @@ CREATE TABLE IF NOT EXISTS `post_draw` (
   `tag` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `author` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `slug` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `date_post` date NOT NULL,
-  PRIMARY KEY (`post_id`)
+  `date_post` date NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -84,9 +73,8 @@ CREATE TABLE IF NOT EXISTS `post_draw` (
 -- Structure de la table `post_text`
 --
 
-DROP TABLE IF EXISTS `post_text`;
-CREATE TABLE IF NOT EXISTS `post_text` (
-  `post_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `post_text` (
+  `post_id` int(11) NOT NULL,
   `post_name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `post_desc` text COLLATE utf8_bin DEFAULT NULL,
   `contenue` longtext COLLATE utf8_bin DEFAULT NULL,
@@ -96,16 +84,16 @@ CREATE TABLE IF NOT EXISTS `post_text` (
   `tag` int(11) DEFAULT NULL,
   `author` varchar(255) COLLATE utf8_bin NOT NULL,
   `slug` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `date_post` date NOT NULL,
-  PRIMARY KEY (`post_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `date_post` date NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Déchargement des données de la table `post_text`
 --
 
 INSERT INTO `post_text` (`post_id`, `post_name`, `post_desc`, `contenue`, `parent_node`, `upvote`, `downvote`, `tag`, `author`, `slug`, `date_post`) VALUES
-(1, 'teste poste ecrit', 'teste poste ecrit', 'Inter has ruinarum varietates a Nisibi quam tuebatur accitus Vrsicinus, cui nos obsecuturos iunxerat imperiale praeceptum, dispicere litis exitialis certamina cogebatur abnuens et reclamans, adulatorum oblatrantibus turmis, bellicosus sane milesque semper et militum ductor sed forensibus iurgiis longe discretus, qui metu sui discriminis anxius cum accusatores quaesitoresque subditivos sibi consociatos ex isdem foveis cerneret emergentes, quae clam palamve agitabantur, occultis Constantium litteris edocebat inplorans subsidia, quorum metu tumor notissimus Caesaris exhalaret.', 1, 666, 234, 1, '1', 'test_post_text', '2020-01-09');
+(1, 'teste poste ecrit', 'teste poste ecrit', 'Inter has ruinarum varietates a Nisibi quam tuebatur accitus Vrsicinus, cui nos obsecuturos iunxerat imperiale praeceptum, dispicere litis exitialis certamina cogebatur abnuens et reclamans, adulatorum oblatrantibus turmis, bellicosus sane milesque semper et militum ductor sed forensibus iurgiis longe discretus, qui metu sui discriminis anxius cum accusatores quaesitoresque subditivos sibi consociatos ex isdem foveis cerneret emergentes, quae clam palamve agitabantur, occultis Constantium litteris edocebat inplorans subsidia, quorum metu tumor notissimus Caesaris exhalaret.', 1, 666, 234, 1, 'zappellin', 'test_post_text', '2020-01-09'),
+(2, 'post teste 2', 'post_test 2', 'Saraceni tamen nec amici nobis umquam nec hostes optandi, ultro citroque discursantes quicquid inveniri poterat momento temporis parvi vastabant milvorum rapacium similes, qui si praedam dispexerint celsius, volatu rapiunt celeri, aut nisi impetraverint, non inmorantur.\r\n', 1, 5589, 33, 1, 'Zappellin', 'post_teste_2', '2020-01-10');
 
 -- --------------------------------------------------------
 
@@ -113,14 +101,12 @@ INSERT INTO `post_text` (`post_id`, `post_name`, `post_desc`, `contenue`, `paren
 -- Structure de la table `tag`
 --
 
-DROP TABLE IF EXISTS `tag`;
-CREATE TABLE IF NOT EXISTS `tag` (
-  `tag_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tag` (
+  `tag_id` int(11) NOT NULL,
   `tag_name` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
   `slug` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
-  `tag_desc` tinytext CHARACTER SET latin1 DEFAULT NULL,
-  PRIMARY KEY (`tag_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `tag_desc` tinytext CHARACTER SET latin1 DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Déchargement des données de la table `tag`
@@ -135,9 +121,8 @@ INSERT INTO `tag` (`tag_id`, `tag_name`, `slug`, `tag_desc`) VALUES
 -- Structure de la table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `Uid` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `Uid` int(11) NOT NULL,
   `username` varchar(255) CHARACTER SET latin1 NOT NULL,
   `password` char(64) CHARACTER SET latin1 NOT NULL,
   `nb_follower` int(11) DEFAULT NULL,
@@ -147,9 +132,8 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(320) CHARACTER SET latin1 DEFAULT NULL,
   `desc` tinytext CHARACTER SET latin1 DEFAULT NULL,
   `website_url` text CHARACTER SET latin1 DEFAULT NULL,
-  `profil_pic` text CHARACTER SET latin1 DEFAULT NULL,
-  PRIMARY KEY (`Uid`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `profil_pic` text CHARACTER SET latin1 DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Déchargement des données de la table `users`
@@ -157,6 +141,81 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`Uid`, `username`, `password`, `nb_follower`, `nb_sub`, `name`, `surname`, `email`, `desc`, `website_url`, `profil_pic`) VALUES
 (1, 'zappellin', '7e513d141be4918489221d6fd0e455d7247d830b522a82701832647b10ab4bfa', NULL, NULL, NULL, NULL, 'guillaume.leon2000@gmail.com', NULL, NULL, NULL);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`post_id`),
+  ADD KEY `tag` (`tag`),
+  ADD KEY `author` (`author`),
+  ADD KEY `post_name` (`post_name`),
+  ADD KEY `post_desc_2` (`post_desc`(1000)),
+  ADD KEY `upvote` (`upvote`),
+  ADD KEY `downvote` (`downvote`),
+  ADD KEY `slug` (`slug`);
+
+--
+-- Index pour la table `post_draw`
+--
+ALTER TABLE `post_draw`
+  ADD PRIMARY KEY (`post_id`);
+
+--
+-- Index pour la table `post_text`
+--
+ALTER TABLE `post_text`
+  ADD PRIMARY KEY (`post_id`);
+
+--
+-- Index pour la table `tag`
+--
+ALTER TABLE `tag`
+  ADD PRIMARY KEY (`tag_id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`Uid`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `post`
+--
+ALTER TABLE `post`
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `post_draw`
+--
+ALTER TABLE `post_draw`
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `post_text`
+--
+ALTER TABLE `post_text`
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `tag`
+--
+ALTER TABLE `tag`
+  MODIFY `tag_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `Uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
