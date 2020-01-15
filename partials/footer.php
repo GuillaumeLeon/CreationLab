a42d4b828c0d19ebef56a9c052121201b731ca7e
Guillaume Leon <guillaume.leon2000@gmail.com>
2020-01-15 22:29:24 +0100

    new footer and menu bar

---
+++ partials/footer.php
@@ -0,0 +1,18 @@
+<footer>
+  <div id="monfooter1"> 
+    <a href="#"> A notre propos </a>
+    <a href="#"> Recrutement </a>
+    <a href="#"> Mention legale</a>
+</div>
+
+<div id="monfooter2"> 
+    <a href="#"> Contact </a>
+    <a href="#"><i class="fab fa-twitter"></i> </a>
+    <a href="https://www.instagram.com/creationlab__/"><i class="fab fa-instagram"></i></a>
+</div>
+
+<div id="monfooter3">
+  Site crée par  <a href="#">WebChemistery</a>
+</div>
+
+</footer>
---
+++ partials/menu.php
@@ -0,0 +1,20 @@
+<div id="menu">
+  <nav class="menu text-center">
+ <ul>
+        <li class="deroulant "><a href="#"> Mon profil </a>
+          <ul class="sous text-center"> 
+            <li id="firstli"> <a href="#"> Profil Détaillé </a></li>
+            <li> <a href="#"> Mes textes </a></li>
+            <li> <a href="#"> Abonnements </a></li>
+            <li> <a href="#"> Mes textes préféres </a></li>
+            <li> <a href="#"> Aléatoire </a></li>
+            <li> <a href="nouvellehistoire.html"> Nouvelle création </a> </li>
+          </ul>
+        </li>
+        <li><a href="#"> Abonnements </a></li>
+        <li><a href="#"> Tendances </a></li>
+        <li><a href="#"> Nouveautés </a></li>
+  </ul> 
+    
+</nav>
+</div
