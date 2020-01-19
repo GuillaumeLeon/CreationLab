1ba73b89a8ccc8e5bb485542eec561a27182a1c0
Guillaume Leon <guillaume.leon2000@gmail.com>
2020-01-19 09:36:06 +0100

    don't remember

--- autre.php
+++ autre.php
@@ -3,7 +3,7 @@ require 'database/db.php';
 
 session_start();
 if ($_SESSION['connected'] != 1) {
-    header('Location:index.php');
+   header('Location:index.php');
 }
 ?>
 <!DOCTYPE html>
@@ -35,7 +35,7 @@ $post = $get_post->fetchAll();
 </div>
 <?php 
 for ($i = count($post) - 1; $i >= 0; $i--) {
-    include 'partials/data_connected.php';
+   include 'partials/data_connected.php';
 }
 ?>
 
