0e7f5dbc1d5a3e600be8bd3573f331a86817ac4e
Guillaume Leon <guillaume.leon2000@gmail.com>
2020-01-20 15:25:18 +0100

    updated database

--- database/creationlab.sql
+++ database/creationlab.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le :  ven. 10 jan. 2020 à 23:14
+-- Généré le :  lun. 20 jan. 2020 à 15:24
 -- Version du serveur :  10.3.18-MariaDB-0+deb10u1
 -- Version de PHP :  7.3.11-1~deb10u1
 
@@ -25,27 +25,17 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `post`
+-- Structure de la table `comment`
 --
 
-CREATE TABLE `post` (
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `post_id` int(11) NOT NULL,
-  `post_name` varchar(255) CHARACTER SET latin1 NOT NULL,
-  `post_desc` text CHARACTER SET latin1 DEFAULT NULL,
-  `upvote` int(11) DEFAULT NULL,
-  `downvote` int(11) DEFAULT NULL,
-  `tag` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `author` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `slug` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
-  `date_post` date NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Déchargement des données de la table `post`
---
-
-INSERT INTO `post` (`post_id`, `post_name`, `post_desc`, `upvote`, `downvote`, `tag`, `author`, `slug`, `date_post`) VALUES
-(3, 'post test 1', 'this is a test post', 543, 213, '1', 'zappellin', 'post test 1', '2020-01-07');
+  `content` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -66,6 +56,13 @@ CREATE TABLE `post_draw` (
   `slug` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `date_post` date NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Déchargement des données de la table `post_draw`
+--
+
+INSERT INTO `post_draw` (`post_id`, `post_name`, `post_desc`, `contenue_url`, `parent_node`, `upvote`, `downvote`, `tag`, `author`, `slug`, `date_post`) VALUES
+(1, 'test image', 'test image\r\n', './testFolder/cX3nrNq.png', NULL, 0, 0, NULL, 'zappellin', NULL, '2020-01-13');
 
 -- --------------------------------------------------------
 
@@ -93,7 +90,26 @@ CREATE TABLE `post_text` (
 
 INSERT INTO `post_text` (`post_id`, `post_name`, `post_desc`, `contenue`, `parent_node`, `upvote`, `downvote`, `tag`, `author`, `slug`, `date_post`) VALUES
 (1, 'teste poste ecrit', 'teste poste ecrit', 'Inter has ruinarum varietates a Nisibi quam tuebatur accitus Vrsicinus, cui nos obsecuturos iunxerat imperiale praeceptum, dispicere litis exitialis certamina cogebatur abnuens et reclamans, adulatorum oblatrantibus turmis, bellicosus sane milesque semper et militum ductor sed forensibus iurgiis longe discretus, qui metu sui discriminis anxius cum accusatores quaesitoresque subditivos sibi consociatos ex isdem foveis cerneret emergentes, quae clam palamve agitabantur, occultis Constantium litteris edocebat inplorans subsidia, quorum metu tumor notissimus Caesaris exhalaret.', 1, 666, 234, 1, 'zappellin', 'test_post_text', '2020-01-09'),
-(2, 'post teste 2', 'post_test 2', 'Saraceni tamen nec amici nobis umquam nec hostes optandi, ultro citroque discursantes quicquid inveniri poterat momento temporis parvi vastabant milvorum rapacium similes, qui si praedam dispexerint celsius, volatu rapiunt celeri, aut nisi impetraverint, non inmorantur.\r\n', 1, 5589, 33, 1, 'Zappellin', 'post_teste_2', '2020-01-10');
+(2, 'post teste 2', 'post_test 2', 'Saraceni tamen nec amici nobis umquam nec hostes optandi, ultro citroque discursantes quicquid inveniri poterat momento temporis parvi vastabant milvorum rapacium similes, qui si praedam dispexerint celsius, volatu rapiunt celeri, aut nisi impetraverint, non inmorantur.\r\n', 1, 5589, 33, 1, 'Zappellin', 'post_teste_2', '2020-01-10'),
+(3, 'test post ecrit ', 'tentative de faire quelque chose ', '\r\n\r\nIpsam vero urbem Byzantiorum fuisse refertissimam atque ornatissimam signis quis ignorat? Quae illi, exhausti sumptibus bellisque maximis, cum omnis Mithridaticos impetus totumque Pontum armatum affervescentem in Asiam atque erumpentem, ore repulsum et cervicibus interclusum suis sustinerent, tum, inquam, Byzantii et postea signa illa et reliqua urbis ornanemta sanctissime custodita tenuerunt.\r\n\r\nEt quoniam apud eos ut in capite mundi morborum acerbitates celsius dominantur, ad quos vel sedandos omnis professio medendi torpescit, excogitatum est adminiculum sospitale nequi amicum perferentem similia videat, additumque est cautionibus paucis remedium aliud satis validum, ut famulos percontatum missos quem ad modum valeant noti hac aegritudine colligati, non ante recipiant domum quam lavacro purgaverint corpus. ita etiam alienis oculis visa metuitur labes.\r\n\r\nHaec et huius modi quaedam innumerabilia ultrix facinorum impiorum bonorumque praemiatrix aliquotiens operatur Adrastia atque utinam semper quam vocabulo duplici etiam Nemesim appellamus: ius quoddam sublime numinis efficacis, humanarum mentium opinione lunari circulo superpositum, vel ut definiunt alii, substantialis tutela generali potentia partilibus praesidens fatis, quam theologi veteres fingentes Iustitiae filiam ex abdita quadam aeternitate tradunt omnia despectare terrena.\r\n', NULL, 0, 0, NULL, 'zappellin', NULL, '2020-01-12'),
+(4, 'hugo le boss', 'hugo est un genie ', 'meme en ayant  3 au partiel il reste meilleur.', NULL, 0, 0, NULL, 'zappellin', NULL, '2020-01-15'),
+(5, 'nouvelle utilisateur', 'qq', 'lorem ipsum dolorem ', NULL, 33, 22, NULL, 'someone', NULL, '2020-01-18'),
+(7, 'poste de kaori', 'ceci est une description de merde', 'j\'ecris des histoire de merde', NULL, 0, 0, NULL, 'kaokao', NULL, '2020-01-20');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `replies`
+--
+
+CREATE TABLE `replies` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `comment_id` int(11) NOT NULL,
+  `content` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -140,24 +156,18 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`Uid`, `username`, `password`, `nb_follower`, `nb_sub`, `name`, `surname`, `email`, `desc`, `website_url`, `profil_pic`) VALUES
-(1, 'zappellin', '7e513d141be4918489221d6fd0e455d7247d830b522a82701832647b10ab4bfa', NULL, NULL, NULL, NULL, 'guillaume.leon2000@gmail.com', NULL, NULL, NULL);
+(1, 'zappellin', '7e513d141be4918489221d6fd0e455d7247d830b522a82701832647b10ab4bfa', NULL, NULL, NULL, NULL, 'guillaume.leon2000@gmail.com', NULL, NULL, NULL),
+(2, 'kaokao', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', NULL, NULL, NULL, NULL, 'kaorilee@outlook.fr', NULL, NULL, NULL);
 
 --
 -- Index pour les tables déchargées
 --
 
 --
--- Index pour la table `post`
+-- Index pour la table `comment`
 --
-ALTER TABLE `post`
-  ADD PRIMARY KEY (`post_id`),
-  ADD KEY `tag` (`tag`),
-  ADD KEY `author` (`author`),
-  ADD KEY `post_name` (`post_name`),
-  ADD KEY `post_desc_2` (`post_desc`(1000)),
-  ADD KEY `upvote` (`upvote`),
-  ADD KEY `downvote` (`downvote`),
-  ADD KEY `slug` (`slug`);
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `post_draw`
@@ -170,6 +180,12 @@ ALTER TABLE `post_draw`
 --
 ALTER TABLE `post_text`
   ADD PRIMARY KEY (`post_id`);
+
+--
+-- Index pour la table `replies`
+--
+ALTER TABLE `replies`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `tag`
@@ -188,22 +204,28 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT pour la table `post`
+-- AUTO_INCREMENT pour la table `comment`
 --
-ALTER TABLE `post`
-  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `comment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `post_draw`
 --
 ALTER TABLE `post_draw`
-  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT pour la table `post_text`
 --
 ALTER TABLE `post_text`
-  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT pour la table `replies`
+--
+ALTER TABLE `replies`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `tag`
@@ -215,7 +237,7 @@ ALTER TABLE `tag`
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `Uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `Uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
