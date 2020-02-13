311658488e44f3ae567366f2b526f6585cbb9fd3
Guillaume Leon <guillaume.leon2000@gmail.com>
2020-02-13 15:10:43 +0100

    remove comics project

--- new_post.php
+++ new_post.php
@@ -15,14 +15,7 @@ if (!isset($_POST['title_post']) && !isset($_POST['content'])) {
     date_default_timezone_set(date_default_timezone_get());
     $date = date('Y/m/d');
 }
-if (isset($_FILES['userfile'])) {
-    $uploaddir = './testFolder/';
-    $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
-    $new_post = $db->prepare('INSERT INTO post_draw (post_name, post_desc,contenue_url, upvote, downvote, author, date_post) VALUES(?,?,?,?,?,?,?)');
-    $new_post->execute(array($title, $desc, $uploadfile, 0, 0, $_SESSION['username'], $date));
-} else {
     $content = $_POST['content'];
     $new_post = $db->prepare('INSERT INTO post_text (post_name, post_desc, contenue, upvote, downvote, author, date_post) VALUES(?,?,?,?,?,?,?)');
     $new_post->execute(array($title, $desc, $content, 0, 0, $_SESSION['username'], $date));
-}
 header('Location:index.php');
