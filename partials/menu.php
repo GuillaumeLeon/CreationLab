2a0506c940dbee8efdb313477246eee3496dd4d6
Guillaume Leon <guillaume.leon2000@gmail.com>
2020-02-26 15:22:32 +0100

    trying to resolve url rewrite pb

--- partials/menu.php
+++ partials/menu.php
@@ -3,11 +3,11 @@
  <ul>
         <li class="deroulant "><a href="#"> Mon profil </a>
           <ul class="sous text-center"> 
-            <li id="firstli"> <a href="users/profil.php"> Profil Détaillé </a></li>
-            <li> <a href="project.php"> Mes projets </a></li>
-            <li> <a href="#"> Abonnements </a></li>
-            <li> <a href="#"> Mes projets préféres </a></li>
-            <li> <a href="#"> Aléatoire </a></li>
+            <li id="firstli"> <a href="http://creationlab.local/users/profil.php"> Profil Détaillé </a></li>
+            <li> <a href="http://creationlab.local/project.php"> Mes projets </a></li>
+            <li> <a href="#"> Abonnement#s </a></li>
+            <li> <a href="#"> Mes projet s préféres </a></li>
+            <li> <a href="#"> Aléatoire #</a></li>
           </ul>
         </li>
         <li><a href="#"> Abonnements </a></li>
